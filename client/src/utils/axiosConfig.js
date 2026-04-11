9476f38591d10cbf37d2cc6d0b4036912b2f98ce
Mallem8704 <mallem@vidyajaya.in>
2026-04-11 14:45:37 +0530

    Fix production API routing

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
+// Set dynamic base URL for production vs local
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
+
 // Set up interceptor to append Token to outgoing requests
 axios.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
